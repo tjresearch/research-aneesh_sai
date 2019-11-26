1258ecfc444370a3fe6b15921ccec8b17666863e
aneesh.boreda92@gmail.com <aneesh.boreda92@gmail.com>
2019-11-26 15:55:17 -0500

    accidentally deleted unity project oops

---
+++ stereotest/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float movementSpeed=2.0f;
+    public float rotationSpeed=1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * movementSpeed), Space.World);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * (Time.deltaTime * movementSpeed), Space.World);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * movementSpeed), Space.World);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * (Time.deltaTime * movementSpeed), Space.World);
+        }
+        if(Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(Vector3.up * (Time.deltaTime * movementSpeed), Space.World);
+        }
+        if(Input.GetKey(KeyCode.X))
+        {
+            transform.Translate(Vector3.down * (Time.deltaTime * movementSpeed), Space.World);
+        }
+        if(Input.GetKey(KeyCode.C))
+        {
+            transform.Rotate(-rotationSpeed,0,0);
+        }
+        if(Input.GetKey(KeyCode.V))
+        {
+            transform.Rotate(rotationSpeed,0,0);
+        }
+        
+        
+    }
+}
---
+++ stereotest/Assets/Scripts/TakePicture.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class TakePicture : MonoBehaviour {
+    
+
+    public string side;
+    public Camera camera;
+    public int resWidth=960;
+    public int resHeight=540;
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            CamCapture();  
+        }
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            string countPath=Application.dataPath+"/StereoImages/"+side+"count.txt";
+            System.IO.File.WriteAllText(countPath, "1");
+            Debug.Log("Reset "+countPath);
+        }
+    }
+    
+    void CamCapture()
+    {
+        string countPath=Application.dataPath+"/StereoImages/"+side+"count.txt";
+        int number = Int32.Parse(System.IO.File.ReadAllText(countPath));
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        camera.targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        Destroy(rt);
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = Application.dataPath + "/StereoImages/" + side + "_" +  number.ToString() + ".png";
+        number++;
+        System.IO.File.WriteAllText(countPath, number.ToString());
+        System.IO.File.WriteAllBytes(filename, bytes);
+        Debug.Log(string.Format("Took screenshot to: {0}", filename));
+
+    }
+    
+}
+
